834a5e74efef51598258ba1d492d3ea123d30ac9
Aditya Kumar Singh <142435507+Axestein@users.noreply.github.com>
2024-09-01 18:11:33 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import weblogo from './weblogo.png';
+import navlogo from './navlogo.png';
+
+export {
+  download,
+  logo,
+  preview,
+  weblogo,
+  navlogo,
+};
